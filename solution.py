a6d691f419dbe8ce20966ab3e5614923193ae09e
LanQikun <lanqikun@github.com>
2022-02-17 18:01:57 +0100

    init

--- solution.py
+++ solution.py
@@ -3,32 +3,26 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
-  #Fill in start
-
-  #Fill in end
-
+  serverSocket.listen(1)
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024).decode()
+
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
-        
+        outputdata = f.read()
+
         #Send one HTTP header line into socket.
-        #Fill in start
-
-        #Fill in end
-
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
           connectionSocket.send(outputdata[i].encode())
@@ -37,16 +31,10 @@ def webServer(port=13331):
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
-        #Fill in start
-
-        #Fill in end
-
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
 
         #Close client socket
-        #Fill in start
-
-        #Fill in end
-
+        connectionSocket.close()
     except (ConnectionResetError, BrokenPipeError):
       pass
 
